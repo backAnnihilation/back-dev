a4a02bf0dc217aca87e6362ae09e9cef97f45167
kotonchiK <anton.mut05@gmail.com>
2024-08-23 20:34:51 +0200

    add swagger notification 5.0, without github and google auth -- fix e2e test

--- apps/user/test/app.e2e-spec.ts
+++ apps/user/test/app.e2e-spec.ts
@@ -21,7 +21,7 @@ describe('AppController (e2e)', () => {
 
   it('/ (GET)', () => {
     return request(app.getHttpServer())
-      .get('/')
+      .get('/helloWorld')
       .expect(200)
       .expect('Hello World!');
   });
