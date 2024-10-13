46699da7e46938d3cdfc2c50255e62824205a1f1
iKlimkin <ghbrot@gmail.com>
2024-10-13 14:30:43 +0300

    add base methods

--- apps/user/src/core/db/base.repository.ts
+++ apps/user/src/core/db/base.repository.ts
@@ -19,4 +19,46 @@ export class BaseRepository {
       throw new Error(`Failed to save ${model as string}: ${error}`);
     }
   }
+
+  async getById<T extends keyof PrismaClient>(
+    model: T,
+    id: string,
+  ): Promise<T | null> {
+    try {
+      return await (this.prisma[model] as any).findUnique({ where: { id } });
+    } catch (error) {
+      console.error(`Failed to get ${model as string}: ${error}`);
+      return null;
+    }
+  }
+
+  async getAll<T extends keyof PrismaClient>(model: T): Promise<T[] | null> {
+    try {
+      return await (this.prisma[model] as any).findMany();
+    } catch (error) {
+      console.error(`Failed to get ${model as string}: ${error}`);
+    }
+  }
+
+  async update(model: keyof PrismaClient, id: string, data: any): Promise<any> {
+    try {
+      return await (this.prisma[model] as any).update({
+        where: { id },
+        data,
+      });
+    } catch (error) {
+      throw new Error(`Failed to update ${model as string}: ${error}`);
+    }
+  }
+
+  async delete<T extends keyof PrismaClient>(
+    model: T,
+    id: string,
+  ): Promise<any> {
+    try {
+      return await (this.prisma[model] as any).delete({ where: { id } });
+    } catch (error) {
+      throw new Error(`Failed to delete ${model as string}: ${error}`);
+    }
+  }
 }
